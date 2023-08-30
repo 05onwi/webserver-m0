aa4dc970bc06cf71519937ecdc41c32d2627da12
william <williamdavidjan@gmail.com>
2023-08-30 08:26:02 +0200

    COMMIT

--- www/public/index.php
+++ www/public/index.php
@@ -1,15 +1,53 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sv">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+    <meta charset="UTF-8" />
+    <title>sudo docker compose up -d sudo docker compose down </title>
 </head>
 <body>
-   <h1>
-      sudo docker compose up -d
-      sudo docker compose down
-   </h1>
-   
+   <header>
+<?php
+   include('inc/header.html');
+   ?>
+
+   </header>
+  
+   <main>
+<h1>Quiz</h1>
+<form action="result.php" method="post">
+    <label>  5-2</label><br>
+    <input type="text" name="q1">
+    <br>
+    <label>5+2</label><br>
+    <input type="text" name="q2">
+    <br>
+    <label>  5-2</label><br>
+    <input type="text" name="q3">
+    <br>
+    <label>  5-2</label><br>
+    <input type="text" name="q4">
+    <br>
+    <label>  5-2</label><br>
+    <input type="text" name="q5">
+    <br>
+    <label>  SKRIV NAMN</label><br>
+    <input type="text" name="name">
+    <br>
+    <input type="submit" value="Skicka">
+
+</form>
+
+
+   </main>
+
+<footer>
+   <?php
+   include('inc/footer.html');
+
+   ?>
+
+</footer>
+
 </body>
 </html>
+
---
+++ www/public/result.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8" />
+    <title>Quiz med Formulär och PHP</title>
+</head>
+<body>
+    <header>
+        <?php
+        include('inc/header.html');
+        ?>
+    </header>
+<h1>Resultat</h1>
+<?php 
+    $points = 0;
+    $ansOne = $_POST['q1'];
+    $ansTwo = $_POST['q2'];
+    $ansThree = $_POST['q3'];
+    $ansFour = $_POST['q4'];
+    $ansFive = $_POST['q5'];
+    $ansName = $_POST['name'];
+
+
+    if($ansOne == '3')
+        $points++;
+    if($ansTwo == '7')
+        $points++;
+    if($ansThree == '3')
+        $points++;
+    if($ansFour == '3')
+        $points++;
+    if($ansFive == '3')
+        $points++;
+
+
+    echo '<strong>Du fick '.$points.' av 6 möjliga. '.$ansName.' </strong>';
+   
+ 
+?>
+
+<footer>
+<footer>
+   <?php
+   include('inc/footer.html');
+
+   ?>
+
+</footer>
+
+</footer>
+</body>
+</html>
+
